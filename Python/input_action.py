eddb5ea83c0142e47f7c6c6e9d3157f8935b6ad1
windeldev <84361797+windeldev@users.noreply.github.com>
2024-05-15 14:24:03 +0800

    Create input_action.py

---
+++ Python/input_action.py
@@ -0,0 +1,31 @@
+import sys
+
+def get_input():
+	command = input("what?: ").split()
+	verb_word = command[0]
+	if verb_word in verb_dict:
+		verb = verb_dict[verb_word]
+	else:
+		print("Unknown verb {}". format(verb_word))
+		return
+
+	if len(command) >= 2:
+		noun_word = command[1]
+		print (verb(noun_word))
+	else:
+		print(verb("nothing"))
+
+def say(noun):
+	return 'You said "{}"'.format(noun)
+
+def exit(noun):
+	sys.exit()
+
+verb_dict = {
+	"say": say,
+	"exit": exit,
+	"bye": exit
+}
+
+while True:
+	get_input()
