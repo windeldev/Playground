bee53a684fb1155e37b1e0bd6c105377bef7b48d
windeldev <windeldev@gmail.com>
2024-05-23 21:37:37 +0800

    feat(lists): list exercise first and last switch - add method slice

--- Python/list_exer_switcher.py
+++ Python/list_exer_switcher.py
@@ -9,9 +9,16 @@ def swap_via_unpack(lst):
         lst = [c, *b, a]
     return lst
 
+def swap_via_slice(lst):
+    if lst:
+        lst = [lst[-1], *lst[1:-1], lst[0]]
+    return lst
+
 
 sample = ['1','7','4','2','8','9']
 sample2 = [6, 5, 8, 0, 1, 3, 3]
+sample3 = ['5', '1']
 
 print(f"indices {swap_via_ind(sample)}")
-print(f"unpacking {swap_via_unpack(sample2)}")
+print(f"unpacking {swap_via_unpack(sample3)}")
+print(f"unpacking {swap_via_slice(sample2)}")
