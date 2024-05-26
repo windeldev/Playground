8512ad37d2f33271d58193f2a058cb8400fbe2fd
windeldev <windeldev@gmail.com>
2024-05-26 21:00:29 +0800

    feat(code exer):  string exercise - print even indexed chars from string, remove 0-n characters from string

--- Python/str_exer_mixed.py
+++ Python/str_exer_mixed.py
@@ -1,10 +1,16 @@
 def print_even_indexed(txt):
-    print(f"Original string is {txt}")
     print("Printing only even index chars")
     for i, val in enumerate(txt):
         if i % 2 ==0:
             print(val)
 
-
+def remove_n_chars(txt):
+    print(f"Removing N..{i} characters from string")
+    i = int(input("Enter number of chars to remove: "))
+    print(txt[i:])
+    
 txt = input("Enter string: ")
+print(f"Original string is {txt}")
 print_even_indexed(txt)
+print("-"*10)
+remove_n_chars(txt)
