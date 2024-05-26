ac9a102cf2e586fa12e9464674876fd89f5b80a0
windeldev <windeldev@gmail.com>
2024-05-26 21:06:49 +0800

    feat(code exer):  string exercise - print even indexed chars from string, remove 0-n characters from string, counting substring from string

--- Python/str_exer_mixed.py
+++ Python/str_exer_mixed.py
@@ -5,12 +5,19 @@ def print_even_indexed(txt):
             print(val)
 
 def remove_n_chars(txt):
-    print(f"Removing N..{i} characters from string")
     i = int(input("Enter number of chars to remove: "))
+    print(f"Removing N..{i} characters from string")
     print(txt[i:])
-    
+
+def count_substr(txt):
+    sub = input("Enter substring to find: ")
+    print(f"Counting {sub} substring from {txt}")
+    print(txt.count(sub))
+
 txt = input("Enter string: ")
 print(f"Original string is {txt}")
 print_even_indexed(txt)
 print("-"*10)
 remove_n_chars(txt)
+print("-"*10)
+count_substr(txt)
