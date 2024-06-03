75a8d5388995e784824eb787bdbdae5a7cefd1a9
windeldev <windeldev@gmail.com>
2024-06-03 14:12:42 +0800

    feat(code exer):  date exercise - str to date - update for sys.argv input

--- Python/str_to_date.py
+++ Python/str_to_date.py
@@ -1,4 +1,11 @@
-import datetime 
+import datetime, sys
+
+str_dt = 'May 30 2024 03:00PM'
+days = 1
+params = sys.argv
+if len(params) > 2:
+    str_dt = params[1]
+    days = int(params[2])
   
 # Function to convert string to datetime 
 def convert(date_time): 
@@ -6,9 +13,13 @@ def convert(date_time):
     datetime_str = datetime.datetime.strptime(date_time, format) 
   
     return datetime_str 
-    
-date_time = 'May 30 2024 03:00PM'
-dt = convert(date_time)
+  
+# Function to add days to target date 
+def add_days(date_time, days_to_add): 
+    return date_time + datetime.timedelta(days=days_to_add) 
+
+print(f"args = {params} str_dt = {str_dt}, days = {days}")
+dt = convert(str_dt)
 print(dt) 
-print(type(convert(date_time)))
-print(dt + datetime.timedelta(days=9))
+print(type(dt))
+print(add_days(dt, days))
