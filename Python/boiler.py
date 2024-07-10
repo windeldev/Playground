281a56079a9b5544f36b6fefd77661bed8b64b9f
windeldev <windeldev@gmail.com>
2024-07-11 06:38:20 +0800

    feat(code exer):  boiler practice

---
+++ Python/boiler.py
@@ -0,0 +1,9 @@
+def greet(name):
+  """Prints a greeting message."""
+  print(f"Hello, {name}!")
+
+if __name__ == "__main__":
+  # Get user input
+  name = input("Enter your name: ")
+  # Call the greet function
+  greet(name)
---
+++ Python/boiler_importer.py
@@ -0,0 +1,6 @@
+import boiler
+
+# Get user input
+name = input("Enter your name: ")
+# Call the greet function
+#boiler.greet(name)
