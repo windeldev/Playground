0d827891b11dce5a2be7d457b8395d82042061a9
windeldev <windeldev@gmail.com>
2024-07-14 18:07:45 +0800

    feat(code exer):  string linear sort

--- Python/linear_sort.py
+++ Python/linear_sort.py
@@ -1,14 +1,20 @@
 import sys
 
 def linear_sort(in_list):
-    out_list = []
-    for i in in_list.split(","):
-        pass
+    lst_len = len(in_list)
+    for i in range(lst_len):
+        for j in range(i+1, lst_len):
+            tmp = ''
+            if in_list[i] > in_list[j]:
+                tmp = in_list[i]
+                in_list[i] = in_list[j]
+                in_list[j] = tmp
+    return in_list
 
 print("Linear Sort:")
 print("Enter comma-delimited list of items to sort:")
 print("Or enter bye to exit:")
-while true:
+while True:
 
     _input = input("Input:")
     if _input.strip().lower() == "bye": sys.exit()
@@ -16,6 +22,6 @@ while true:
         if _input.find(",") > -1: break
         else:
             print("Enter comma-delimited list of items to sort")
-
-linear_sort(_input)
+print("Before:",_input)
+print("After:",linear_sort(_input.split(",")))
 
